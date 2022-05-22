95d1ca5dcb913b48592b52e1a98a0a806f0846a1
José Luís Martins <jlmartins@inesc-mn.pt>
2022-05-22 16:13:54 +0100

    version 6.0.8 validation improvements

--- Validation/run.sh
+++ Validation/run.sh
@@ -1,15 +1,27 @@
 #/bin/bash
 
+# path to executable you want to test
+
+executable="../Src/atom_all_ifort.exe"
+
+# list of all named list_elements
+
 input="list_elements"
+
+# initialize the file with a summary of the test
+
+cat /dev/null > ptb.out
 
 while IFS= read -r element
 
 do
 
-../Src/atom_all_ifort.exe $element
+echo processing $element
+
+$executable $element >> ptb.out
 
 mkdir res_$element
 
-mv *.gp *.pdf atom.out *.dat *.UPF *.DAT psd.pot res_$element 
+mv *.gp *.pdf atom.out *.dat *.UPF *.DAT psd.pot res_$element
 
 done  < $input
